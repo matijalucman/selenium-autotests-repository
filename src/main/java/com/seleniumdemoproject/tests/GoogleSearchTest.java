e6114ebe8a63ddab5762c701d917181fc4194082
matijalucman <matija.lucman@hotmail.com>
2017-06-18 20:12:02 +0200

    Added new test. Added confiration for surefile plugin.

--- src/main/java/com/seleniumdemoproject/tests/GoogleSearchTest.java
+++ src/main/java/com/seleniumdemoproject/tests/GoogleSearchTest.java
@@ -5,15 +5,31 @@ import com.seleniumdemoproject.pageobjectmodel.GoogleMainPage;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
-public class GoogleSearch_Test extends TestBase {
+public class GoogleSearchTest extends TestBase {
 
     @Test
-    public void GoogleSearch(){
+    public void googleSearchSelenium_Test(){
 
         try {
             GoogleMainPage googleMainPage = GoogleMainPage.usingDriverNavigateTo(driver, "https://www.google.hr");
 
             googleMainPage.searchByInsertedValue("selenium");
+
+            Assert.assertTrue(googleMainPage.isSearchSuccessful());
+        }
+        catch (Exception e) {
+            System.out.println("There was an error during test execution: " + e);
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void googleSearchApple_Test(){
+
+        try {
+            GoogleMainPage googleMainPage = GoogleMainPage.usingDriverNavigateTo(driver, "https://www.google.hr");
+
+            googleMainPage.searchByInsertedValue("apple");
 
             Assert.assertTrue(googleMainPage.isSearchSuccessful());
         }
